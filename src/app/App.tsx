230fd79a07f940a7ed986dcb4492e4b4f168e5ba
FreeHill <nernuer@gmail.com>
2020-10-29 11:52:56 +0000

    initial commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,18 +2,20 @@ import React from 'react';
 import './App.css';
 import { Router, Route } from 'react-router-dom'
 import { createBrowserHistory } from 'history'
-import Routes from '../routes'
+// import Routes from '../routes'
 
 const history = createBrowserHistory()
 const App: React.FC = () => {
   return (
-    // <Router history={history}>
-    //   <Routes />
-    // </Router>
+
     <Router history={history}>
-      <Route exact path="/" render={() => (window.location.href = "https://www.pigeonbolt.com")} />
+      <Route exact path="/" render={() => (window.location.href = "https://newsletters.pigeonbolt.com")} />
     </Router>
   );
 }
 
 export default App;
+
+    // <Router history={history}>
+    //   <Routes />
+    // </Router>
