3b7bada02cf5844e3730b6f9e40838d56f41dc77
Fredrick Obeng-Nyarko <frederickon@nyeova.com>
2020-10-30 08:32:01 +0000

    fix errors

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import HomePage from './pages/home/index'
+// import HomePage from './pages/home/index'
 import { Redirect, Switch } from 'react-router-dom'
-import { RouteWithLayout } from './_shared/components'
-import { Main as MainLayout } from './_shared/Layouts'
+// import { RouteWithLayout } from './_shared/components'
+// import { Main as MainLayout } from './_shared/Layouts'
 const Routes = () => {
     return (
         <Switch>
-            <RouteWithLayout component={HomePage} exact layout={MainLayout} path='/' />
+            {/* <RouteWithLayout component={HomePage} exact layout={MainLayout} path='/' /> */}
             <Redirect to='not-found' />
         </Switch>
     )
